8fd0c0ddc8f579baefd7856314a3f51b206636f3
Danic <Danic@DESKTOP-556539A>
2023-10-21 14:13:10 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace UrnaAV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCargoParaVoto = new System.Windows.Forms.Label();
+            this.txtVoto = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnBranco = new System.Windows.Forms.Button();
+            this.btnCorrige = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCargoParaVoto
+            // 
+            this.lblCargoParaVoto.AutoSize = true;
+            this.lblCargoParaVoto.BackColor = System.Drawing.Color.Black;
+            this.lblCargoParaVoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargoParaVoto.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCargoParaVoto.Location = new System.Drawing.Point(210, 9);
+            this.lblCargoParaVoto.MaximumSize = new System.Drawing.Size(391, 61);
+            this.lblCargoParaVoto.MinimumSize = new System.Drawing.Size(391, 61);
+            this.lblCargoParaVoto.Name = "lblCargoParaVoto";
+            this.lblCargoParaVoto.Padding = new System.Windows.Forms.Padding(12);
+            this.lblCargoParaVoto.Size = new System.Drawing.Size(391, 61);
+            this.lblCargoParaVoto.TabIndex = 0;
+            this.lblCargoParaVoto.Text = "DEPUTADO FEDERAL";
+            this.lblCargoParaVoto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCargoParaVoto.Click += new System.EventHandler(this.lblCargoParaVoto_Click);
+            // 
+            // txtVoto
+            // 
+            this.txtVoto.BackColor = System.Drawing.Color.Black;
+            this.txtVoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVoto.ForeColor = System.Drawing.Color.White;
+            this.txtVoto.Location = new System.Drawing.Point(311, 70);
+            this.txtVoto.Margin = new System.Windows.Forms.Padding(0);
+            this.txtVoto.MaxLength = 5;
+            this.txtVoto.Name = "txtVoto";
+            this.txtVoto.ReadOnly = true;
+            this.txtVoto.Size = new System.Drawing.Size(182, 44);
+            this.txtVoto.TabIndex = 1;
+            this.txtVoto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(220, 139);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(50, 50);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(311, 139);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(50, 50);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(405, 139);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(50, 50);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(220, 210);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(50, 50);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(311, 210);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(50, 50);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(405, 210);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(50, 50);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(220, 282);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(50, 50);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(311, 282);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(50, 50);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(405, 282);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(50, 50);
+            this.btn9.TabIndex = 10;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(311, 355);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(50, 50);
+            this.btn0.TabIndex = 11;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnBranco
+            // 
+            this.btnBranco.BackColor = System.Drawing.Color.White;
+            this.btnBranco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBranco.ForeColor = System.Drawing.Color.Black;
+            this.btnBranco.Location = new System.Drawing.Point(480, 139);
+            this.btnBranco.Name = "btnBranco";
+            this.btnBranco.Size = new System.Drawing.Size(105, 50);
+            this.btnBranco.TabIndex = 12;
+            this.btnBranco.Text = "BRANCO";
+            this.btnBranco.UseVisualStyleBackColor = false;
+            this.btnBranco.Click += new System.EventHandler(this.btnBranco_Click);
+            // 
+            // btnCorrige
+            // 
+            this.btnCorrige.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCorrige.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCorrige.ForeColor = System.Drawing.Color.White;
+            this.btnCorrige.Location = new System.Drawing.Point(480, 210);
+            this.btnCorrige.Name = "btnCorrige";
+            this.btnCorrige.Size = new System.Drawing.Size(105, 50);
+            this.btnCorrige.TabIndex = 13;
+            this.btnCorrige.Text = "CORRIGE";
+            this.btnCorrige.UseVisualStyleBackColor = false;
+            this.btnCorrige.Click += new System.EventHandler(this.btnCorrige_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.ForeColor = System.Drawing.Color.White;
+            this.btnEnviar.Location = new System.Drawing.Point(480, 282);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(105, 50);
+            this.btnEnviar.TabIndex = 14;
+            this.btnEnviar.Text = "CONFIRMAR";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnCorrige);
+            this.Controls.Add(this.btnBranco);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtVoto);
+            this.Controls.Add(this.lblCargoParaVoto);
+            this.Name = "Form1";
+            this.Text = "Urna Eletrônica";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCargoParaVoto;
+        private System.Windows.Forms.TextBox txtVoto;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnBranco;
+        private System.Windows.Forms.Button btnCorrige;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UrnaAV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblCargoParaVoto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if(txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 1;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 2;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 3;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 4;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 5;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 6;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 7;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 8;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 9;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (txtVoto.Text.Length <= 4)
+            {
+                txtVoto.Text += 0;
+            }
+            else
+            {
+                txtVoto.Text = txtVoto.Text;
+            }
+        }
+
+        private void btnBranco_Click(object sender, EventArgs e)
+        {
+            txtVoto.Text = "BRANCO";
+        }
+
+        String votoDepFederal = "";
+        String votoDepEstOuDist = "";
+        String votoSenador = "";
+        String votoGovernador = "";
+        String votoPresidente = "";
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+
+            if(txtVoto.Text == "")
+            {
+                MessageBox.Show("Antes de confirmar o seu voto insira o numero do seu candidato," +
+                    " caso opite por anulá-lo aperte o botão BRANCO.",
+                    "Verifique o seu voto",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (votoDepFederal == "")
+                {
+                    votoDepFederal = txtVoto.Text;
+                    txtVoto.Text = "";
+                    lblCargoParaVoto.Text = "Deputado Estadual/Distrial";
+                }
+                else if (votoDepEstOuDist == "")
+                {
+                    votoDepEstOuDist = txtVoto.Text;
+                    txtVoto.Text = "";
+                    lblCargoParaVoto.Text = "Senador";
+                }
+                else if (votoSenador == "")
+                {
+                    votoSenador = txtVoto.Text;
+                    txtVoto.Text = "";
+                    lblCargoParaVoto.Text = "Governador";
+                }
+                else if(votoGovernador == "")
+                {
+                    votoGovernador = txtVoto.Text;
+                    txtVoto.Text = "";
+                    lblCargoParaVoto.Text = "Presidente";
+                }
+                else if(votoPresidente == "")
+                {
+                    votoPresidente = txtVoto.Text;
+                    txtVoto.Text = "";
+                    votoDepFederal = "";
+                    votoDepEstOuDist = "";
+                    votoSenador = "";
+                    votoGovernador = "";
+                    votoPresidente = "";
+
+                    lblCargoParaVoto.Text = "Deputado Federal";
+                }
+            }
+        }
+
+        private void btnCorrige_Click(object sender, EventArgs e)
+        {
+            txtVoto.Text = "";
+        }
+    }
+}
